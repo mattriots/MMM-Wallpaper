0a5a0e432949e7519a0b3c15ed4f440cb3054578
mattriots <mattdepauw@gmail.com>
2024-12-30 12:37:15 -0800

    first iteration of the imageNav feature

--- MMM-Wallpaper.js
+++ MMM-Wallpaper.js
@@ -1,243 +1,312 @@
 // MMM-Wallpaper.js
 
-Module.register("MMM-Wallpaper", {
+Module.register('MMM-Wallpaper', {
   // Default module config
   defaults: {
-    source: "bing",
+    source: 'bing',
     updateInterval: 60 * 60 * 1000,
     slideInterval: 5 * 60 * 1000,
     maximumEntries: 10,
-    filter: "grayscale(0.5) brightness(0.5)",
-    orientation: "auto",
+    filter: 'grayscale(0.5) brightness(0.5)',
+    orientation: 'auto',
     caption: true,
     crossfade: true,
     maxWidth: Number.MAX_SAFE_INTEGER,
     maxHeight: Number.MAX_SAFE_INTEGER,
     nsfw: true,
-    size: "cover",
+    size: 'cover',
     shuffle: true,
     addCacheBuster: true,
-    userPresenceAction: "none",
+    userPresenceAction: 'none',
     fillRegion: true,
-    width: "auto",
-    height: "auto",
-    flickrApiKey: "",
+    width: 'auto',
+    height: 'auto',
+    flickrApiKey: '',
     flickrDataCacheTime: 24 * 60 * 60 * 1000,
     flickrResultsPerPage: 500, // Flickr API is limited to 500 photos per page
     fadeEdges: false,
+    navImage: false, // Config option for navigation between images
   },
 
-  getStyles: function() {
-    return ["MMM-Wallpaper.css"];
+  getStyles: function () {
+    return ['MMM-Wallpaper.css']
   },
 
-  start: function() {
-    var self = this;
+  start: function () {
+    var self = this
 
-    self.loadNextImageTimer = null;
-    self.imageIndex = -1;
+    self.loadNextImageTimer = null
+    self.imageIndex = -1
 
-    self.wrapper = document.createElement("div");
-    self.wrapper.className = "MMM-Wallpaper";
+    self.wrapper = document.createElement('div')
+    self.wrapper.className = 'MMM-Wallpaper'
 
-    self.content = document.createElement("div");
-    self.wrapper.appendChild(self.content);
+    self.content = document.createElement('div')
+    self.wrapper.appendChild(self.content)
 
-    self.title = document.createElement("div");
-    self.title.className = "title";
-    self.content.appendChild(self.title);
+    self.title = document.createElement('div')
+    self.title.className = 'title'
+    self.content.appendChild(self.title)
 
     if (self.config.fadeEdges) {
-      self.topGradient = document.createElement("div");
-      self.topGradient.className = "top-gradient";
-      self.content.appendChild(self.topGradient);
+      self.topGradient = document.createElement('div')
+      self.topGradient.className = 'top-gradient'
+      self.content.appendChild(self.topGradient)
 
-      self.bottomGradient = document.createElement("div");
-      self.bottomGradient.className = "bottom-gradient";
-      self.content.appendChild(self.bottomGradient);
+      self.bottomGradient = document.createElement('div')
+      self.bottomGradient.className = 'bottom-gradient'
+      self.content.appendChild(self.bottomGradient)
     }
 
     if (self.config.fillRegion) {
-      self.content.className = "content-fill";
+      self.content.className = 'content-fill'
     } else {
-      self.content.className = "content";
-      self.content.style.width = self.config.width;
-      self.content.style.height = self.config.height;
+      self.content.className = 'content'
+      self.content.style.width = self.config.width
+      self.content.style.height = self.config.height
     }
 
-    self.imageElement = null;
-    self.nextImageElement = null;
+    self.imageElement = null
+    self.nextImageElement = null
 
-    self.getData();
-    self.updateTimer = setInterval(() => self.getData(), self.config.updateInterval);
+    self.getData()
+    self.updateTimer = setInterval(
+      () => self.getData(),
+      self.config.updateInterval
+    )
+
+    //Added event listener for keydown (left = previous and right = next)
+    //Conditional check to see if navImage is enabled in the config
+    if (self.config.navImage) {
+      window.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+          self.loadNextImage('previous')
+        } else if (event.key === 'ArrowRight') {
+          self.loadNextImage('next')
+        }
+      })
+    }
   },
 
-  notificationReceived: function(notification, payload, sender) {
-    var self = this;
+  notificationReceived: function (notification, payload, sender) {
+    var self = this
 
-    if (notification === "MODULE_DOM_CREATED" && self.config.userPresenceAction === "show") {
-      self.hide();
-    } else if (notification === "LOAD_NEXT_WALLPAPER") {
-      self.loadNextImage();
-    } else if (notification === "USER_PRESENCE") {
-      if (self.config.userPresenceAction === "show") {
-        payload ? self.show() : self.hide();
-      } else if (self.config.userPresenceAction === "hide") {
-        payload ? self.hide() : self.show();
+    if (
+      notification === 'MODULE_DOM_CREATED' &&
+      self.config.userPresenceAction === 'show'
+    ) {
+      self.hide()
+    } else if (notification === 'LOAD_NEXT_WALLPAPER') {
+      self.loadNextImage('next')
+    } else if (notification === 'USER_PRESENCE') {
+      if (self.config.userPresenceAction === 'show') {
+        payload ? self.show() : self.hide()
+      } else if (self.config.userPresenceAction === 'hide') {
+        payload ? self.hide() : self.show()
       }
-    } else if (notification === "UPDATE_WALLPAPER_CONFIG") {
+    } else if (notification === 'UPDATE_WALLPAPER_CONFIG') {
       if (payload instanceof String) {
-        self.config.source = payload;
+        self.config.source = payload
       } else {
-        Object.assign(self.config, payload);
+        Object.assign(self.config, payload)
       }
-
-      clearInterval(self.updateTimer);
-      self.getData();
-      self.updateTimer = setInterval(() => self.getData(), self.config.updateInterval);
+      clearInterval(self.updateTimer)
+      self.getData()
+      self.updateTimer = setInterval(
+        () => self.getData(),
+        self.config.updateInterval
+      )
     }
   },
 
-  socketNotificationReceived: function(notification, payload) {
-    var self = this;
+  socketNotificationReceived: function (notification, payload) {
+    var self = this
 
-    if (notification === "WALLPAPERS") {
-      if (payload.orientation === self.getOrientation() &&
-          ((Array.isArray(self.config.source) && self.config.source.includes(payload.source)) ||
-           (!Array.isArray(self.config.source) && self.config.source === payload.source)))
-      {
-        self.images = payload.images.slice(0, self.config.maximumEntries);
-        self.imageIndex = self.imageIndex % (self.images.length || 1);
+    if (notification === 'WALLPAPERS') {
+      if (
+        payload.orientation === self.getOrientation() &&
+        ((Array.isArray(self.config.source) &&
+          self.config.source.includes(payload.source)) ||
+          (!Array.isArray(self.config.source) &&
+            self.config.source === payload.source))
+      ) {
+        self.images = payload.images.slice(0, self.config.maximumEntries)
+        self.imageIndex = self.imageIndex % (self.images.length || 1)
 
         if (self.imageElement === null && self.images.length > 0) {
-          self.loadNextImage();
+          self.loadNextImage('next')
         }
       }
     }
   },
 
-  getData: function() {
-    var self = this;
-    var config = Object.assign({}, self.config);
+  getData: function () {
+    var self = this
+    var config = Object.assign({}, self.config)
 
-    config.orientation = self.getOrientation();
-    self.sendSocketNotification("FETCH_WALLPAPERS", config);
+    config.orientation = self.getOrientation()
+    self.sendSocketNotification('FETCH_WALLPAPERS', config)
   },
 
-  getOrientation: function() {
-    var self = this;
+  getOrientation: function () {
+    var self = this
 
-    if (self.config.orientation === "auto") {
-      var viewport = self.getViewport();
-      return (viewport.width < viewport.height) ? "vertical" : "horizontal";
+    if (self.config.orientation === 'auto') {
+      var viewport = self.getViewport()
+      return viewport.width < viewport.height ? 'vertical' : 'horizontal'
     }
 
-    return self.config.orientation;
+    return self.config.orientation
   },
 
-  onImageLoaded: function(imageData, element) {
-    var self = this;
+  onImageLoaded: function (imageData, element) {
+    var self = this
 
     return () => {
-      self.resetLoadImageTimer();
+      self.resetLoadImageTimer()
 
-      element.className = `wallpaper ${self.config.crossfade ? "crossfade-image" : ""}`;
-      element.style.opacity = 1;
-      self.title.style.display = "none";
+      element.className = `wallpaper ${self.config.crossfade ? 'crossfade-image' : ''}`
+      element.style.opacity = 1
+      self.title.style.display = 'none'
 
-      setTimeout(() => {
-        var caption = imageData.caption;
-        if (self.config.caption && caption) {
-          self.title.innerHTML = caption;
-          self.title.style.display = "initial";
-        }
+      setTimeout(
+        () => {
+          var caption = imageData.caption
+          if (self.config.caption && caption) {
+            self.title.innerHTML = caption
+            self.title.style.display = 'initial'
+          }
 
-        if (self.imageElement !== null) {
-          self.content.removeChild(self.imageElement);
-        }
-        self.imageElement = self.nextImageElement;
-        self.nextImageElement = null;
-      }, self.config.crossfade ? 1000 : 0);
-    };
+          if (self.imageElement !== null) {
+            self.content.removeChild(self.imageElement)
+          }
+          self.imageElement = self.nextImageElement
+          self.nextImageElement = null
+        },
+        self.config.crossfade ? 1000 : 0
+      )
+    }
   },
 
-  createImage: function(imageData) {
-    var self = this;
-    var img = document.createElement("img");
+  createImage: function (imageData) {
+    var self = this
+    var img = document.createElement('img')
 
-    img.style.filter = self.config.filter;
-    img.style["object-fit"] = self.config.size;
-    img.style.opacity = 0;
-    img.onload = self.onImageLoaded(imageData, img);
-    img.src = self.getImageUrl(imageData);
+    img.style.filter = self.config.filter
+    img.style['object-fit'] = self.config.size
+    img.style.opacity = 0
+    img.onload = self.onImageLoaded(imageData, img)
+    img.src = self.getImageUrl(imageData)
 
-    return img;
+    return img
   },
 
-  getDom: function() {
-    return this.wrapper;
+  getDom: function () {
+    return this.wrapper
   },
 
-  getViewport: function() {
-    var w = window;
-    var e = document.documentElement;
-    var g = document.body;
+  getViewport: function () {
+    var w = window
+    var e = document.documentElement
+    var g = document.body
 
     return {
       width: w.innerWidth || e.clientWidth || g.clientWidth,
-      height: w.innerHeight || e.clientHeight || g.clientHeight
-    };
+      height: w.innerHeight || e.clientHeight || g.clientHeight,
+    }
   },
 
-  getImageUrl: function(image) {
-    var viewport = this.getViewport();
-    var url = image.url;
+  getImageUrl: function (image) {
+    var viewport = this.getViewport()
+    var url = image.url
 
-    if ("variants" in image) {
+    if ('variants' in image) {
       for (var i in image.variants) {
-        var variant = image.variants[i];
+        var variant = image.variants[i]
 
-        if (variant.width > this.config.maxWidth || variant.height > this.config.maxHeight) {
-          break;
+        if (
+          variant.width > this.config.maxWidth ||
+          variant.height > this.config.maxHeight
+        ) {
+          break
         }
 
-        url = variant.url;
+        url = variant.url
 
-        if (variant.width >= viewport.width && variant.height >= viewport.height) {
-          break;
+        if (
+          variant.width >= viewport.width &&
+          variant.height >= viewport.height
+        ) {
+          break
         }
       }
     }
 
-    return url;
+    return url
   },
 
-  loadNextImage: function() {
-    var self = this;
+  loadNextImage: function (direction) {
+    var self = this
 
-    self.resetLoadImageTimer();
+    self.resetLoadImageTimer()
 
     if (self.nextImageElement !== null) {
-      self.nextImageElement.onload = null;
-      self.content.removeChild(self.nextImageElement);
-      self.nextImageElement = null;
+      self.nextImageElement.onload = null
+      self.content.removeChild(self.nextImageElement)
+      self.nextImageElement = null
     }
 
-    self.imageIndex = (self.imageIndex + 1) % self.images.length;
+    // Increment or decrement the image index based on the direction
+    this.imageIndex =
+      direction === 'next'
+        ? (this.imageIndex + 1) % this.images.length
+        : (this.imageIndex - 1 + this.images.length) % this.images.length
 
-    const nextImageData = self.images[self.imageIndex];
+    // self.imageIndex = (self.imageIndex + 1) % self.images.length
+
+    const nextImageData = self.images[self.imageIndex]
     if (nextImageData !== null) {
-      self.nextImageElement = self.createImage(nextImageData);
-      self.content.insertBefore(self.nextImageElement, self.title);
+      self.nextImageElement = self.createImage(nextImageData)
+      self.content.insertBefore(self.nextImageElement, self.title)
     }
   },
 
-  resetLoadImageTimer: function() {
-    const self = this;
+  // Added this to allow navigation between images using the arrow keys
+
+  // navigateImage: function(direction) {
+  //   var self = this;
+
+  //   self.resetLoadImageTimer();
+
+  //   if (self.nextImageElement !== null) {
+  //     self.nextImageElement.onload = null;
+  //     self.content.removeChild(self.nextImageElement);
+  //     self.nextImageElement = null;
+  //   }
+
+  // // Increment or decrement the image index based on the direction
+  // if (direction === "next") {
+  //   self.imageIndex = (self.imageIndex + 1) % self.images.length;
+  // } else if (direction === "previous") {
+  //   self.imageIndex = (self.imageIndex - 1 + self.images.length) % self.images.length;
+  // }
+
+  //   const previousImageData = self.images[self.imageIndex];
+  //   if (previousImageData !== null) {
+  //     self.nextImageElement = self.createImage(previousImageData);
+  //     self.content.insertBefore(self.nextImageElement, self.title);
+  //   }
+  // },
+
+  resetLoadImageTimer: function () {
+    const self = this
 
     if (self.config.slideInterval > 0) {
-      clearTimeout(self.loadNextImageTimer);
-      self.loadNextImageTimer = setTimeout(() => self.loadNextImage(), self.config.slideInterval);
+      clearTimeout(self.loadNextImageTimer)
+      self.loadNextImageTimer = setTimeout(
+        () => self.loadNextImage('next'),
+        self.config.slideInterval
+      )
     }
   },
-});
+})
